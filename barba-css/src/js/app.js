7445b03b6c30be65d060b343c83062cdf2dd3174
Alena <alena.nik.digital@gmail.com>
2020-09-26 20:12:21 +0200

    global hook to avoid white flash

--- barba-css/src/js/app.js
+++ barba-css/src/js/app.js
@@ -2,6 +2,17 @@ import barba from '@barba/core';
 import barbaCss from '@barba/css';
 
 barba.use(barbaCss);
+const body = document.querySelector('body');
+
+// global hook that runs before any transition
+barba.hooks.before((data) => {
+
+  const background = data.current.container.dataset.background;
+  body.style.setProperty('---page-background', background);
+
+})
+
+
 barba.init({
   transitions: [
     {
@@ -15,6 +26,14 @@ barba.init({
       afterOnce(data) {
 
       }
+    },
+    {
+      name: 'fade',
+      to: {
+        namespace: ['fade']
+      },
+      leave() {},
+      enter() {}
     },
   ],
 });
