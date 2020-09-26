37df2c7660ec61083c13b11a858d5e0eb59fb5af
Alena <alena.nik.digital@gmail.com>
2020-09-26 18:51:29 +0200

    fading

--- barba-css/src/js/app.js
+++ barba-css/src/js/app.js
@@ -1,1 +1,20 @@
+import barba from '@barba/core';
+import barbaCss from '@barba/css';
 
+barba.use(barbaCss);
+barba.init({
+  transitions: [
+    {
+      name: 'home',
+      beforeOnce(data) {
+
+      },
+      once() {
+
+      },
+      afterOnce(data) {
+
+      }
+    },
+  ],
+});
