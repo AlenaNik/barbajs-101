ba615fd3a19475a5a2a85bf8c0aa3032dea53cd9
Alena <alena.nik.digital@gmail.com>
2020-09-27 11:45:09 +0200

    staggering links

--- barba-gsap/src/js/app.js
+++ barba-gsap/src/js/app.js
@@ -1,11 +1,23 @@
 import barba from '@barba/core';
+import gsap from 'gsap';
 import { animationEnter, animationLeave } from './animations'
 
+const resetActiveLink = () => gsap.set('a.is-active span', {
+  xPercent: -100,
+  transformOrigin: 'left',
+})
 
 barba.init({
   transitions: [
     {
       once({ next }) {
+        resetActiveLink()
+        gsap.from('header a', {
+          duration: 0.6,
+          yPercent: 100,
+          stagger: 0.2,
+          ease: 'power1.out',
+        })
         animationEnter(next.container);
       },
       leave: ({ current }) => {
